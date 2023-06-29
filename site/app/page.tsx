cff418be17805f7d4a654ea9e01e3285a15764d2
Adrian Chen <absszero@gmail.com>
2023-06-29 12:14:20 +0800

    add jest

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -4,6 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
