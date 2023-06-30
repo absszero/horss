9dcee55aafa9a142b76cfd13c56327d350dd7e76
Adrian Chen <absszero@gmail.com>
2023-06-30 16:25:34 +0800

    refactor

--- site/lib/CacheFetch.ts
+++ site/lib/CacheFetch.ts
@@ -1,18 +1,26 @@
 import Cache from 'file-system-cache';
 import { resolve } from 'path';
-import getConfig from 'next/config'
+import getConfig from 'next/config';
+import { pino } from 'pino';
 
-export default async (link: string) => {
+export default async (link: string, doCache : boolean = true) => {
     const { serverRuntimeConfig } = getConfig()
     const cache = Cache({
-        basePath: resolve('./' + '.cahce'),
+        basePath: resolve('./pages', '../' + serverRuntimeConfig.CACHE_DIR),
         ttl: 172800, // 快取兩天
-      });
+    });
+    let logger = pino();
+    logger.info(`get cache: ${link}`);
     let text = await cache.get(link, undefined);
     if (undefined === text) {
+        logger.info(`fetch: ${link}`);
         const response = await fetch(link);
         text = await response.text();
-        cache.set(link, text);
+
+        if (doCache) {
+            logger.info(`cache: ${link}`);
+            cache.set(link, text);
+        }
     }
 
     return text;
--- site/lib/Xml.ts
+++ site/lib/Xml.ts
@@ -1,5 +1,6 @@
 import { NextApiResponse } from 'next'
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
+import { parse as htmlParse } from 'node-html-parser';
 
 export function setXmlHeader ( res: NextApiResponse )  {
     res.setHeader("Content-Type", "application/xml;charset=UTF-8");
@@ -13,4 +14,10 @@ export function build ( json: any )  {
     return (new XMLBuilder()).build(json);
 }
 
-export default { setXmlHeader, parse, build }
+export function toSafeXml(html: string, selector: string) {
+    const root = htmlParse(html);
+    const article = root.querySelector(selector);
+    return article?.toString().replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+}
+
+export default { setXmlHeader, parse, build, toSafeXml }
