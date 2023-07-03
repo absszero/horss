bf94c4944b000781a8f1d51349e48443642b20d2
Adrian Chen <absszero@gmail.com>
2023-07-03 10:44:15 +0800

    fix empty text return

--- site/lib/CacheFetch.ts
+++ site/lib/CacheFetch.ts
@@ -6,18 +6,18 @@ export default async (link: string, doCache : boolean = true) => {
     let logger = pino();
     logger.info(`get cache: ${link}`);
     let text = await kv.get(link);
-    if(!text) {
-        logger.info(`fetch: ${link}`);
-        const response = await fetch(link);
-        text = await response.text();
-
-        if (doCache) {
-            logger.info(`set cache: ${link}`);
-            await kv.set(link, text);
-        }
-
+    if(text) {
         return text as string;
     }
 
-    return '';
+    logger.info(`fetch: ${link}`);
+    const response = await fetch(link);
+    text = await response.text();
+
+    if (doCache) {
+        logger.info(`set cache: ${link}`);
+        await kv.set(link, text);
+    }
+
+    return text as string;
 }
