cb3da817422b4e61f2b9bf1d446f9550c7430b01
Adrian Chen <absszero@gmail.com>
2023-07-03 10:37:30 +0800

    use vercel/kv to store cache

--- site/lib/CacheFetch.ts
+++ site/lib/CacheFetch.ts
@@ -1,27 +1,23 @@
-import Cache from 'file-system-cache';
-import { resolve } from 'path';
-import getConfig from 'next/config';
 import { pino } from 'pino';
+import { kv } from "@vercel/kv";
 
 export default async (link: string, doCache : boolean = true) => {
-    const { serverRuntimeConfig } = getConfig()
-    const cache = Cache({
-        basePath: resolve('./pages', '../.cache'),
-        ttl: 172800, // 快取兩天
-    });
+
     let logger = pino();
     logger.info(`get cache: ${link}`);
-    let text = await cache.get(link, undefined);
-    if (undefined === text) {
+    let text = await kv.get(link);
+    if(!text) {
         logger.info(`fetch: ${link}`);
         const response = await fetch(link);
         text = await response.text();
 
         if (doCache) {
-            logger.info(`cache: ${link}`);
-            cache.set(link, text);
+            logger.info(`set cache: ${link}`);
+            await kv.set(link, text);
         }
+
+        return text as string;
     }
 
-    return text;
+    return '';
 }
