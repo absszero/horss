e65fd23f171288dc567d98a6d6b9ed3864e19411
Adrian Chen <absszero@gmail.com>
2025-02-01 17:51:36 +0800

    add greenhore

--- site/lib/Xml.ts
+++ site/lib/Xml.ts
@@ -2,15 +2,15 @@ import { NextApiResponse } from 'next'
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 import { parse as htmlParse } from 'node-html-parser';
 
-export function setXmlHeader ( res: NextApiResponse )  {
+export function setXmlHeader( res: NextApiResponse )  {
     res.setHeader("Content-Type", "application/xml;charset=UTF-8");
 }
 
-export function parse ( xml: string, options?: {} )  {
+export function parse( xml: string, options?: {} )  {
     return (new XMLParser(options)).parse(xml);
 }
 
-export function build ( json: any, options?: {} )  {
+export function build( json: any, options?: {} )  {
     return (new XMLBuilder(options)).build(json);
 }
 
--- site/pages/api/greenhorn.ts
+++ site/pages/api/greenhorn.ts
@@ -3,19 +3,19 @@ import Xml from '@/lib/Xml';
 import Storage from '@/lib/Storage';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const rss = 'https://crossing.cw.com.tw/rss';
+  const rss = 'https://www.blogger.com/feeds/1264116278467470539/posts/default';
   const storage = new Storage;
   const xml = await storage.fetch(rss, false);
 
-  const options = {ignoreAttributes: false, allowBooleanAttributes: true, suppressBooleanAttributes: false};
+  const options = {ignoreAttributes: false, suppressBooleanAttributes: false};
   const json = Xml.parse(xml, options);
 
-  const selectors = ['div.article-box__main', 'article'];
-  for (const item of json.rss.channel.item) {
-    const html = await storage.fetch(item.link);
+  const selectors = ['#main-wrapper'];
+  for (const item of json.feed.entry) {
+    const html = await storage.fetch(item.link[4]['@_href']);
     for (const selector of selectors) {
-      item.description = Xml.toSafeXml(html, selector);
-      if (item.description) break;
+      item.summary['#text'] = Xml.toSafeXml(html, selector);
+      if (item.summary['#text']) break;
     }
   }
   const xmlContent = Xml.build(json, options);
